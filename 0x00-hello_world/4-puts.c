48bfb1656a9d1ffed84e4ab3750e4a209e51c2bb
Frank Success <frank.c.success@gmail.com>
2022-04-07 16:21:48 -0700

    Getting started with C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * prints exactly '"Programming is like building a multilingual puzzle, followed by a new line.'
+ * returns 0 if compiled successfully, and non-zero if an error occurs
+ */
+
+int main ()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * prints exactly 'with proper grammar, but the outcome is a piece of art,' followed by a new line.
+ * returns 0 if successful, othewise non-zero
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
