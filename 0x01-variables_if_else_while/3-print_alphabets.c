ba15828f2ffc436608bee325382ed421fd0c28e1
Frank Success <frank.c.success@gmail.com>
2022-04-08 16:38:36 -0700

    Update tasks 3,5,6

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,11 @@
 int main(void)
 {
 	char alpha;
-	for (alpha=‘a’; alpha<=‘z’; alpha++)
+
+	for (alpha = ‘a’; alpha <= ‘z’; alpha++)
 		putchar(alpha);
-	for (alpha=‘A’; alpha<=‘Z’; alpha++)
+
+	for (alpha = ‘A’; alpha <= ‘Z’; alpha++)
 		putchar(alpha);
 	putchar('\n');
 	return (0);
