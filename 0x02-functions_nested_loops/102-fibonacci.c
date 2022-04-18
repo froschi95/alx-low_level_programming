081177a27a75de9d961f7f1ea7de497a1eae2f37
Frank Success <frank.c.success@gmail.com>
2022-04-18 15:26:00 -0700

    Update advanced tasks

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2,
+ * followed by a new line
+ * Return: 0 Always for Success
+ */
+
+int fibonach(int i);
+
+int main(void)
+{
+	int i;
+
+	for (i=1; i<=50; i++)
+	{
+		if (i != 50)
+		{
+			printf("%d, ", fibonach(i));
+		}
+		else {
+			printf("%d\n", fibonach(i));
+		}
+	}
+}
+
+int fibonach(int i)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return fibonach(i-1) + fibonach(i-2);
+	}
+}
