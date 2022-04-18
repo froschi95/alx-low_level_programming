387edbd43e0cb2882efa8ba25be24de2e7b83979
Frank Success <frank.c.success@gmail.com>
2022-04-18 11:50:43 -0700

    Advanced tasks

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table starting with 0,
+ * if n is greater that 15 or less than zero it prints nothing
+ * @n: any integer argument
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+	int l, m, p, tens, units;
+
+	if (n >= 0 && n < 15)
+	{
+		l = 0;
+		while (l <= n)
+		{
+			m = 0;
+			while (m <= n)
+			{
+				p = l * m;
+				tens = p / 10;
+				units = p % 10;
+
+				if (m == 0)
+				{
+					_putchar('0');
+				}
+				else if (p < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(units + '0');
+				}
+				else if (p >= 10 && p < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((tens % 10) + '0');
+					_putchar(units + '0');
+				}
+				else if ( p > 99 && p < 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(p / 100 + '0');
+					_putchar(tens % 10 + '0');
+					_putchar(units + '0');
+				}
+				m++;
+			}
+			_putchar('\n');
+			l++;
+		}
+	}
+}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - prints the sum of all multiples of 3 and 5 below 1024
+* Return: 0 Always for Success
+*/
+
+int main(void)
+{
+	unsigned long int sum_3s, sum_5s, total;
+	int n;
+	
+	sum_3s = sum_5s = total = 0;
+	
+	while (n < 1024)
+	{
+		if (n % 3 == 0)
+		{
+			sum_3s = sum_3s + n;
+		}
+		else if (n % 5 == 0)
+		{
+			sum_5s = sum_5s + n;
+		}
+		n++;
+	}
+	total = sum_3s + sum_5s;
+	printf("%lu\n", total);
+	return (0);
+}
