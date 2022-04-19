dd659879b8700278966e58e02f54bfd25470e7c6
Frank Success <frank.c.success@gmail.com>
2022-04-19 13:08:18 -0700

    Update tasks 7&8

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,26 @@
 
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n <= 0)
 		_putchar('\n');
 	for (i = 1; i <= n; i++)
 	{
-		_putchar('\\');
-		_putchar('\n');
+		if (i == 1)
+		{
+			_putchar('\\');
+			_putchar('\n');
+			continue;
+		}
+		else
+		{
+			for (j = 1; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 }
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -20,5 +20,5 @@ void print_square(int size)
 		}
 		_putchar('\n');
 	}
-	_putchar('\n')'
+	_putchar('\n');
 }
