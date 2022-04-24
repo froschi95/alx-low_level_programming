137fa5bd8ab9f67a932d10ee2ce0d223afbcab37
Frank Success <frank.c.success@gmail.com>
2022-04-24 16:56:59 -0700

    Add tasks 4,5,6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to be capitalized
+ * Return: pointer to s
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char ch[] = {9, 10, 32, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 97 && s[i] <=122)
+		{
+			s[i] = s[i] - 32;
+			continue;
+		}
+		for (j = 0; ch[j] != '\0'; j++;)
+		{
+			if (ch[j] == s[i])
+			{
+				++i;
+				if (s[i] >= 97 && s[i] <= 122)
+				{
+					s[i] = s[i] - 32;
+					break;
+				}
+			}
+		}
+		if (s[i] >= 65  && s[i] <= 90)
+		{
+			s[i] = s[i] + 32;
+		}
+	}
+	return (s);
+}
