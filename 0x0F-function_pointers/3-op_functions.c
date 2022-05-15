5d3469f604a74fcbe044a873ee4deabac982aabe
Frank Success <frank.c.success@gmail.com>
2022-05-15 16:13:45 -0700

    Function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - Adds two numbers
+ * @a: int
+ * @b: int
+ * Return: int
+ **/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - finds the difference of two numbers
+ * @a: int
+ * @b: int
+ * Return: int
+ **/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two numbers
+ * @a: int
+ * @b: int
+ * Return: int
+ **/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides two numbers
+ * @a: int
+ * @b: int
+ * Return: int
+ **/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Finds the Modulo two numbers
+ * @a: int
+ * @b: int
+ * Return: returns the remainder or modulo
+ **/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
