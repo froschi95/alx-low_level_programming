87470f452731447abc5e52e120fe24e91835ba15
froschi95 <60326840+froschi95@users.noreply.github.com>
2022-07-08 00:19:54 +0100

    Add task

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: head of linked list
+ * Return: nada
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+
+dlistint_t *temp;
+
+while (head)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
+}
