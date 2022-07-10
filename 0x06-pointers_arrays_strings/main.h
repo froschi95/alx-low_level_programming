363dd9fc45bfb341565267ab686ddb25edc1e9e7
froschi95 <60326840+froschi95@users.noreply.github.com>
2022-07-10 10:53:21 +0100

    Update

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
-char *leet(char *str);
+char *leet(char *);
 char *rot13(char *str);
 void print_number(int n);
 int _putchar(char c);
