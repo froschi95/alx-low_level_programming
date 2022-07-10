01b050c09da9ac9cc7e06564195576de7d888a0e
froschi95 <60326840+froschi95@users.noreply.github.com>
2022-07-10 09:53:05 +0100

    Removed trailing space

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -5,7 +5,7 @@
  * @h: The head of the dlistint_t list
  * Return: The number of elements in the linked dlistint_t list
  */
- 
+
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t num_nodes = 0;
