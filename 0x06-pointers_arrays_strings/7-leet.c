9657e34c77cdd405c5c0ba08156f610fc2673d6e
froschi95 <60326840+froschi95@users.noreply.github.com>
2022-07-10 10:49:53 +0100

    Update task

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,4 +8,22 @@
 
 char *leet(char *s)
 {
+    char *cp = c;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+	unsigned int i;
+
+	while (*c)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+			if (*c == key[i] || *c == key[i] + 32)
+			{
+				*c = 48 + value[i];
+			}
+		}
+		c++;
+	}
+
+	return (cp);
 }
