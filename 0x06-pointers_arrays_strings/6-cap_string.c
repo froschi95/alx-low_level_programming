7e156d70e8f859530e324a2650e5accda03b9346
froschi95 <60326840+froschi95@users.noreply.github.com>
2022-07-10 11:00:34 +0100

    Update task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,31 +8,30 @@
 
 char *cap_string(char *s)
 {
-	int i, j;
-	char ch[] = {9, 10, 32, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+int i;
+int prev;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (i == 0 && s[i] >= 97 && s[i] <= 122)
-		{
-			s[i] = s[i] - 32;
-			continue;
-		}
-		for (j = 0; ch[j] != '\0'; j++)
-		{
-			if (ch[j] == s[i])
-			{
-				if (s[i + 1] >= 97 && s[i + 1] <= 122)
-				{
-					s[i + 1] = s[i + 1] - 32;
-					break;
-				}
-			}
-		}
-		if (s[i] >= 65  && s[i] <= 90)
-		{
-			s[i] = s[i] + 32;
-		}
-	}
-	return (s);
+for (i = 0; s[i] != '\0'; i++)
+{
+prev = i - 1;
+if (s[i] >= 'a' && s[i] <= 'z')
+{
+if (i == 0)
+s[i] = s[i] - 32;
+else if (s[prev] >= 9 && s[prev] <= 10)
+s[i] = s[i] - 32;
+else if (s[prev] >= 32 && s[prev] <= 34)
+s[i] = s[i] - 32;
+else if (s[prev] >= 40 && s[prev] <= 41)
+s[i] = s[i] - 32;
+else if (s[prev] == 46)
+s[i] = s[i] - 32;
+else if (s[prev] == 59)
+s[i] = s[i] - 32;
+else if (s[prev] == 123 || s[prev] == 125)
+s[i] = s[i] - 32;
+}
+}
+
+return (s);
 }
