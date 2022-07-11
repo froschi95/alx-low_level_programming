f907b6ed918d5f1ade52cc583b83b6c46dbc0fcf
froschi95 <60326840+froschi95@users.noreply.github.com>
2022-07-11 01:51:42 +0100

    Fix trailing white spaces

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,9 +4,9 @@
  * create_file - creates a file
  * @filename: filename
  * @text_content: content writed in the file
- * Return: 1 for success, otherwise -1 
+ * Return: 1 for success, otherwise -1
  */
- 
+
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
