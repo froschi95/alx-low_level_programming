3acea6fb943a35685389c2924ed028b627b05454
Frank Success <frank.c.success@gmail.com>
2022-09-19 10:49:27 -0700

    add tasks

---
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Entry point
+ * @s: input string
+ * @accept: input
+ * Return: char
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int k;
+
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+		if (*s == accept[k])
+		return (s);
+		}
+	s++;
+	}
+
+return ('\0');
+}
