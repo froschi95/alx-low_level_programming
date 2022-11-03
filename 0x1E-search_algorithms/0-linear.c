7397a761e987e15b824ebf98403d9346cf0f3544
Frank Success <60326840+froschi95@users.noreply.github.com>
2022-11-03 12:17:42 +0100

    Update task

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,13 +10,14 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	unsigned int i;
+	int i;
 
 	if (array == NULL)
 		return (-1);
-	for (i = 0; i < size; i++)
+
+	for (i = 0; i < (signed int)size; i++)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		printf("Value checked array[%i] = [%i]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
